14009f25aa7cb25118574b0d9bc149ddac7c22fb
Rifa Achrinza <25147899+achrinza@users.noreply.github.com>
2020-01-23 12:32:20 +0800

    feat: add nextjs concept demo
    Signed-off-by: Rifa Achrinza <25147899+achrinza@users.noreply.github.com>

---
+++ concept-demo/nextjs/pages/index.tsx
@@ -0,0 +1,22 @@
+import Hero from './hero';
+import Head from 'next/head';
+
+export default function Homepage() {
+    return (
+        <>
+            <Head>
+                <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" key="font-roboto-500" />
+                <title>Hello, Werld</title>
+            </Head>
+
+            <Hero src="https://images.unsplash.com/photo-1531113463068-6f334622d795?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60" />
+            <Hero src="https://images.unsplash.com/photo-1540073280202-6e5c781befec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60" />
+            <h1>Hello, Welrd!</h1>
+            <style jsx>{`
+                .h1 {
+                    font-family: 'Roboto';
+                }
+            `}</style>
+        </>
+    );
+}

4e2e4ea36a15a2fa8582e678e9fe83619be7697c
Jun Hao Lim <s10195387@connect.np.edu.sg>
2020-01-24 10:04:20 +0800

    fix: fix hero image and text position
    Signed-off-by: Jun Hao Lim <s10195387@connect.np.edu.sg>

--- concept-demo/nextjs/.next/static/runtime/webpack.js
+++ concept-demo/nextjs/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e5a7ebfe134940a455da";
+/******/ 	var hotCurrentHash = "91c79aaeb055a1eaca12";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

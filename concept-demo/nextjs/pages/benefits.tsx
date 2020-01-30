ad8833cef607a2b32b2501deb962b38084aa54ac
Alan Tan <dodieboy@users.noreply.github.com>
2020-01-31 00:52:29 +0800

    Update benefits.tsx
    * Remove test.tsx

--- concept-demo/nextjs/pages/benefits.tsx
+++ concept-demo/nextjs/pages/benefits.tsx
@@ -11,7 +11,9 @@ export default function Benefits() {
                 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" key="js-jquery3"></script>
                 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" key="css-bootstrap4" />
                 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" key="js-bootstrap4"></script>
-                <link href="https://fonts.googleapis.com/css?family=Bungee|Oswald&display=swap" rel="stylesheet" key="font-bungee"/>
+                <link href="https://fonts.googleapis.com/css?family=Anton|Bungee|Oswald&display=swap" rel="stylesheet" key="font-bungee"/>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" key="css-animate" />
+                <script async src="/static/js/scroll-left.js" />
             </Head>
 
             <Header />
@@ -45,36 +47,43 @@ export default function Benefits() {
                     <img className="poly" src="https://images.unsplash.com/photo-1463740839922-2d3b7e426a56?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=819&q=80" alt="Unable to fetch image"></img>
                 </div>
                 <div className="row bg3">
-                    <h2 className="text-center">HAVING A BALANCED DIET</h2>    
-                    <div className="col-md food-box1">
-                        <h5>Eating and drinking the right things</h5>
-                        <p>It can be hard to keep a healthy diet. Especially when there's a temptation to eat sweets and junk food. But it's important to get a balance in your diet of the right amount of each food group.</p>
-                        <p>A balanced diet also includes drinks. Your body needs to stay hydrated to keep healthy. But think about what you drink. Caffeine and sugary drinks:</p>
-                        <ul>
-                            <li>can cause mood swings</li>
-                            <li>affect your energy levels, feeling full of energy before a "sugar low" where you may feel tired and grumpy</li>
-                            <li>can make you put on weight if you have too much sugar</li>
-                            <li>can give you trouble sleeping if you have them at night</li>
-                        </ul>
+                    <h2 className="text-center">HAVING A BALANCED DIET</h2>
+                    <div className="scroll-animations">
+                        <div className="col-md food-box1 animated">
+                            <h5>Eating and drinking the right things</h5>
+                            <p>It can be hard to keep a healthy diet. Especially when there's a temptation to eat sweets and junk food. But it's important to get a balance in your diet from each food group.</p>
+                            <p>A balanced diet also includes drinks. Your body needs to stay hydrated to keep healthy. But when drinking Caffeine and sugary drinks:</p>
+                            <ul>
+                                <li>can cause mood swings</li>
+                                <li>affect your energy levels, feeling full of energy before a "sugar low" where you may feel tired and grumpy</li>
+                                <li>can make you put on weight if you have too much sugar</li>
+                                <li>can give you trouble sleeping if you have them at night</li>
+                            </ul>
+                        </div>
+                        <div className="col-md food-box2 animated">
+                            <h5>Eating and drinking the right things</h5>
+                            <p>It can be hard to keep a healthy diet. Especially when there's a temptation to eat sweets and junk food. But it's important to get a balance in your diet of the right amount of each food group.</p>
+                            <p>A balanced diet also includes drinks. Your body needs to stay hydrated to keep healthy. But think about what you drink. Caffeine and sugary drinks:</p>
+                            <ul>
+                                <li>can cause mood swings</li>
+                                <li>affect your energy levels, feeling full of energy before a "sugar low" where you may feel tired and grumpy</li>
+                                <li>can make you put on weight if you have too much sugar</li>
+                                <li>can give you trouble sleeping if you have them at night</li>
+                            </ul>
+                        </div>
                     </div>
-                    <div className="col-md food-box2">
-                        <h5>Eating and drinking the right things</h5>
-                        <p>It can be hard to keep a healthy diet. Especially when there's a temptation to eat sweets and junk food. But it's important to get a balance in your diet of the right amount of each food group.</p>
-                        <p>A balanced diet also includes drinks. Your body needs to stay hydrated to keep healthy. But think about what you drink. Caffeine and sugary drinks:</p>
-                        <ul>
-                            <li>can cause mood swings</li>
-                            <li>affect your energy levels, feeling full of energy before a "sugar low" where you may feel tired and grumpy</li>
-                            <li>can make you put on weight if you have too much sugar</li>
-                            <li>can give you trouble sleeping if you have them at night</li>
-                        </ul>
-                    </div>
+                    
 
 
                 </div>
             </div>
 
             <Footer />
+
             <style jsx>{`
+                body{
+                    overflow-x: hidden;
+                }
                 .container-fluid{
                     background-color: #d8f1ea;
                     padding: 0;
@@ -110,7 +119,7 @@ export default function Benefits() {
                     width: 100%;
                 }
                 h5, h2 {
-                    font-family: 'Oswald', sans-serif;
+                    font-family: 'Anton';
                 }
                 .hero-text{
                     background-color: rgba(0,0,0, 0.65);
@@ -145,7 +154,7 @@ export default function Benefits() {
                 }
                 .food-box1, .food-box2{
                     margin-top: 20px;
-                    padding: 50px;
+                    padding: 60px;
                     box-shadow: 0 30px 55px 0 rgba(0, 0, 0, 0.3), 0 20px 20px 0 rgba(0, 0, 0, 0.15);
                 }
                 .food-box1{
@@ -155,6 +164,27 @@ export default function Benefits() {
                 .food-box2{
                     margin-left: 10px;
                     background-color: #cc33ff;
+                }
+                .scroll-animations {
+                    display: flex;
+                }
+
+                .scroll-animations div {
+                    flex-grow: 1;
+                    opacity: 0;
+                }
+
+                @media (max-width: 759px) {
+                    .row{
+                        padding: 40px 30px;
+                    }
+                    .scroll-animations {
+                        display: block;
+                    }
+                    .food-box1, .food-box2{
+                        margin: 15px 0;
+                        padding: 15px 40px;
+                    }
                 }
             `}</style>
         </>

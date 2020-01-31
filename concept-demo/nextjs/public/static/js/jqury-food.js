ad5211c3ba6ebc9be1da5023d0e0821cce9a302b
Jun Hao Lim <s10195387@connect.np.edu.sg>
2020-01-31 16:25:46 +0800

    fix: fix text and image scaling
    Signed-off-by: Jun Hao Lim <s10195387@connect.np.edu.sg>

--- concept-demo/nextjs/public/static/js/jqury-food.js
+++ concept-demo/nextjs/public/static/js/jqury-food.js
@@ -1,33 +1,34 @@
 
-    $('#carbs').hide();
-    $('#meat').hide();
+    
     
     $(document).ready(function() {
+        $('#carbs').hide();
+        $('#meat').hide();
         $('#veggie').off().click(function() {
             if ($('#meat').is(':visible')){
-                $('#meat').fadeOut(100);
+                $('#meat').fadeOut('slow');
             }
             if($('#carbs').is(':visible')) {
-                $('#carbs').fadeOut(100);
+                $('#carbs').fadeOut('slow');
             }
-            $('#veg').delay(700).fadeIn(1000);
+            $('#veg').delay('slow').fadeIn('slow');
         })
         $('#carbos').off().click(function() {
             if ($('#meat').is(':visible')){
-                $('#meat').fadeOut(100);
+                $('#meat').fadeOut('slow');
             }
             if ($('#veg').is(':visible')){
-                $('#veg').fadeOut(100);
+                $('#veg').fadeOut('slow');
             }
-            $('#carbs').delay(700).fadeIn(1000);
+            $('#carbs').delay('slow').fadeIn('slow');
         })
         $('#protein').off().click(function() {
             if ($('#veg').is(':visible')){
-                $('#veg').fadeOut(100);
+                $('#veg').fadeOut('slow');
             }
             if ($('#carbs').is(':visible')){
-                $('#carbs').fadeOut(100);
+                $('#carbs').fadeOut('slow');
             }
-            $('#meat').delay(700).fadeIn(1000);
+            $('#meat').delay('slow').fadeIn('slow');
         })
     })

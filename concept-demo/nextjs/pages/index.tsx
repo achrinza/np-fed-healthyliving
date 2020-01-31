f831bcd91398984ec085a0db3c5a70902852a3e2
Jun Hao Lim <s10195387@connect.np.edu.sg>
2020-01-31 18:26:15 +0800

    feat: add images
    Signed-off-by: Jun Hao Lim <s10195387@connect.np.edu.sg>

--- concept-demo/nextjs/pages/index.tsx
+++ concept-demo/nextjs/pages/index.tsx
@@ -13,28 +13,23 @@ export default function Homepage() {
             </Head>
 
             <Header />
-
-            <video className="hero" src="/static/media/running.mov" autoPlay muted loop />
-
-            <Sectionr type="left" imgUrl="/static/media/homepage-hero.jpg">
+            <Sectionr type="left" imgUrl="https://images.unsplash.com/photo-1536181783029-1097aaf179de?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1052&q=80">
                 <h2>Programmes</h2>
                 <p>Get to know more of the many fun activities provided by the government, most free of charge!</p>
                 <a href="/programmes">Let's seem <abbr title="them">'em</abbr>!</a>
             </Sectionr>
 
-            <Sectionr type="right" imgUrl="/static/media/homepage-hero.jpg">
+            <Sectionr type="right" imgUrl="https://images.unsplash.com/photo-1541534741688-6078c6bfb5c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1049&q=80">
                 <h2>Activities</h2>
                 <p>Get to know what activities you can do to keep yourself healthy at a low cost!</p>
                 <a href="/activities">Let's seem <abbr title="them">'em</abbr>!</a>
             </Sectionr>
 
-            <Sectionr type="left" imgUrl="/static/media/homepage-hero.jpg">
+            <Sectionr type="left" imgUrl="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80">
                 <h2>Healthy eating</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <a href="/food">Let's seem <abbr title="them">'em</abbr>!</a>
             </Sectionr>
-
-            <Footer />
 
             <style jsx>{`
                 .h1 {

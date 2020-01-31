dea1e43e7b8f50f03be3f4f288098e31d68f0ddb
dodieboy <dodieboy@users.noreply.github.com>
2020-01-31 15:07:39 +0800

    Alan (#26)
    * Create benefits.tsx
    
    * Create footer.tsx
    
    * Update footer.tsx
    
    * test file
    
    * Update benefits.tsx
    
    * Update benefits.tsx
    
    * Remove test.tsx
    
    * Remove test.tsx
    
    * Added survey (not complete)
    
    * Fix typo in footer
    
    * Update footer.tsx
    
    * External css for Footer
    
    * Update benefits.tsx

--- concept-demo/nextjs/public/static/js/scroll-left.js
+++ concept-demo/nextjs/public/static/js/scroll-left.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
       var docViewBottom = docViewTop + $(window).height();
   
       var elemTop = $(elem).offset().top;
-      var elemBottom = elemTop + $(elem).height();
+      var elemBottom = elemTop + ($(elem).height()/2);
   
       return elemBottom <= docViewBottom && elemTop >= docViewTop;
     }
---
+++ concept-demo/nextjs/public/static/js/survey.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    var score = 0;
+    $('#q2').hide();
+    $('#q3').hide();
+    $('#q4').hide();
+    $('#q5').hide();
+    $(".next-btn").off().click(function() {
+        var id = parseInt($(this).val())
+        if(id == 1 && ($('#q1-value').val() == '' || $('#q1-value').val() == null)){
+                $('#q1-error').text('Please enter your name')
+        }
+        else{
+            var inputId = '#q' + id + '-value';
+            $('#q1-error').text('');
+            if(id < 4 && id > 1){
+                score += parseInt($(inputId).val());
+            }
+            else{
+                $('#result-title').text('Hey ' + $('#q1-value').val());
+                if(score < 6){
+                    $('#result').text('You are very healthy');
+                }
+                else{
+                    $('#result').text("It's time for you to go exercise and have a balance diet");
+                }
+            }
+            if(id < 5){
+                var id1 = '#q'+ id;
+                var id2 = '#q'+ (id + 1);
+                console.log("bb")
+                $(id1).addClass('fadeOutRight').delay(500).queue(function(next){
+                    console.log("aa")
+                    $(id1).hide().removeClass('fadeInLeft').removeClass('fadeOutRight');
+                    $(id2).addClass('fadeInLeft').show();
+                    next();
+                });
+            }
+        }
+    });
+    $(".reset").off().click(function() {
+        score = 0;
+        $('#q5').addClass('fadeOutRight').delay(500).queue(function(next){
+            $('#q5').hide();
+            $('#q1').addClass('fadeInLeft').show();
+            $('#q5').removeClass('fadeOutRight');
+            next();
+        });
+    });
+});

2360ad5f15242e1b2d0da45eccb67869f0619b55
Say Ping Neo <s10196448@connect.np.edu.sg>
2020-01-31 22:38:46 +0800

    final

--- concept-demo/nextjs/pages/activities.tsx
+++ concept-demo/nextjs/pages/activities.tsx
@@ -34,9 +34,10 @@ export default function Homepage() {
                 </Hero>
 
                 <div className="block-hero-transitioner-wrapper">
-                    <svg className="block-hero-transitioner" xmlns="http://www.w3.org/2000/svg" height="320px" width="100%" preserveAspectRatio="none" viewBox="0 0 1440 320">
-                        <path fill="#00cba9" fillOpacity="1" d="M0,160L48,186.7C96,213,192,267,288,250.7C384,235,480,149,576,138.7C672,128,768,192,864,213.3C960,235,1056,213,1152,197.3C1248,181,1344,171,1392,165.3L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
-                    </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" height="320px" width="100%" preserveAspectRatio="none" viewBox="0 0 1440 320">
+                    <path fill="#0099ff" fill-opacity="1" d="M0,128L48,117.3C96,107,192,85,288,80C384,75,480,85,576,106.7C672,128,768,160,864,192C960,224,1056,256,1152,266.7C1248,277,1344,267,1392,261.3L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+                </svg>
+
                 </div>
             </div>
             <div className="explaination">

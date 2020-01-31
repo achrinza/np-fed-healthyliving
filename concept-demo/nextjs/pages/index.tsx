595a157f64da19eeb7eb3c3fa476eb232524dbc5
AlephaLIm <51186021+AlephaLIm@users.noreply.github.com>
2020-01-31 22:43:51 +0800

    Feat/indexing (#41)
    * fix: fix food header
    
    Signed-off-by: Jun Hao Lim <s10195387@connect.np.edu.sg>
    
    * feat: update food with parallaxr
    
    Signed-off-by: Jun Hao Lim <s10195387@connect.np.edu.sg>
    
    * feat: update contents
    
    Signed-off-by: Jun Hao Lim <s10195387@connect.np.edu.sg>
    
    * feat: update food.tsx
    
    Signed-off-by: Jun Hao Lim <s10195387@connect.np.edu.sg>
    
    * fix: fix text and image scaling
    
    Signed-off-by: Jun Hao Lim <s10195387@connect.np.edu.sg>
    
    * feat: add images
    
    Signed-off-by: Jun Hao Lim <s10195387@connect.np.edu.sg>
    
    * Remove debug msg from feedback-prompt.js
    
    * Revert "Remove debug msg from feedback-prompt.js"
    
    This reverts commit e500eb7a3c3badc2ae38f3135a1e36af3d171a5f.
    
    * fix: incomplete fix of index
    
    Signed-off-by: Jun Hao Lim <s10195387@connect.np.edu.sg>
    
    Co-authored-by: dodieboy <dodieboy@users.noreply.github.com>

--- concept-demo/nextjs/pages/index.tsx
+++ concept-demo/nextjs/pages/index.tsx
@@ -13,28 +13,26 @@ export default function Homepage() {
             </Head>
 
             <Header />
-
             <video className="hero" src="/static/media/running.mov" autoPlay muted loop />
+            <div className="body_sections">
+                <Sectionr type="left" imgUrl="https://images.unsplash.com/photo-1536181783029-1097aaf179de?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1052&q=80" style="max-height: 60vh;">
+                        <h2>Programmes</h2>
+                        <p>Get to know more of the many fun activities provided by the government, most free of charge!</p>
+                        <a href="/programmes">Let's seem <abbr title="them">'em</abbr>!</a>
+                </Sectionr>
 
-            <Sectionr type="left" imgUrl="/static/media/homepage-hero.jpg">
-                <h2>Programmes</h2>
-                <p>Get to know more of the many fun activities provided by the government, most free of charge!</p>
-                <a href="/programmes">Let's seem <abbr title="them">'em</abbr>!</a>
-            </Sectionr>
+                <Sectionr type="right" imgUrl="https://images.unsplash.com/photo-1517649763962-0c623066013b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" style="max-height: 60vh;">
+                    <h2>Activities</h2>
+                    <p>Get to know what activities you can do to keep yourself healthy at a low cost!</p>
+                    <a href="/activities">Let's seem <abbr title="them">'em</abbr>!</a>
+                </Sectionr>
 
-            <Sectionr type="right" imgUrl="/static/media/homepage-hero.jpg">
-                <h2>Activities</h2>
-                <p>Get to know what activities you can do to keep yourself healthy at a low cost!</p>
-                <a href="/activities">Let's seem <abbr title="them">'em</abbr>!</a>
-            </Sectionr>
-
-            <Sectionr type="left" imgUrl="/static/media/homepage-hero.jpg">
-                <h2>Healthy eating</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <a href="/food">Let's seem <abbr title="them">'em</abbr>!</a>
-            </Sectionr>
-
-            <Footer />
+                <Sectionr type="left" imgUrl="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" style="max-height: 60vh;">
+                    <h2>Healthy eating</h2>
+                    <p>Gain the essentials to start planning your healthy diet! No More Searching! It's all here!</p>
+                    <a href="/food">Let's seem <abbr title="them">'em</abbr>!</a>
+                </Sectionr>
+            </div>
 
             <style jsx>{`
                 .h1 {
@@ -45,6 +43,12 @@ export default function Homepage() {
                     height: 100vh;
                     width: 100%;
                     object-fit: cover;
+                }
+                .body_sections {
+                    margin-top: -5px;
+                    background-color: #0D1140;
+                    color: #FAF7DB;
+                    text-shadow: 2px 2px 4px #061E1F;
                 }
             `}</style>
         </>

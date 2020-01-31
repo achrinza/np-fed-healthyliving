c9793a8f7ce16628df3e1ad0f7f6e58ecec6de5d
SayPing02 <60082482+SayPing02@users.noreply.github.com>
2020-01-31 16:25:32 +0800

    Feat/activities (#29)
    * form
    
    * completed
    
    * new  header

--- concept-demo/nextjs/pages/activities.tsx
+++ concept-demo/nextjs/pages/activities.tsx
@@ -22,16 +22,22 @@ export default function Homepage() {
                 <link rel="stylesheet" href="/static/css/activities.css"/>
             </Head>
             <Header/>
-            <div className="hero-wrapper">
-                <Hero  className="heroimg" src="/static/media/activities-header.jpg" style="height: 50vh;">
-                    <div className="hero-text">
-                        <h1> 
-                            Activities
-                            <br/>
-                            Healthy Body, Healthy Mind
-                        </h1>
+            <div className="block-hero-wrapper">
+                <Hero src="/static/media/activities-header.jpg" style="position: relative; height: 100vh;">
+                    <div className="block-hero__backdrop" />
+                    <div className="block-hero__textblock">
+                        <div className="block-hero__textblock-group">
+                            <h1 className="block-hero__title">Activities</h1>
+                            <p className="block-hero__desc">Healthy Body, Healthy Mind</p>
+                        </div>
                     </div>
                 </Hero>
+
+                <div className="block-hero-transitioner-wrapper">
+                    <svg className="block-hero-transitioner" xmlns="http://www.w3.org/2000/svg" height="320px" width="100%" preserveAspectRatio="none" viewBox="0 0 1440 320">
+                        <path fill="#00cba9" fillOpacity="1" d="M0,160L48,186.7C96,213,192,267,288,250.7C384,235,480,149,576,138.7C672,128,768,192,864,213.3C960,235,1056,213,1152,197.3C1248,181,1344,171,1392,165.3L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+                    </svg>
+                </div>
             </div>
             <div className="explaination">
                 <h1 className="explainh1">Why we should we take part in physical activities</h1>
@@ -187,7 +193,7 @@ export default function Homepage() {
                             <br/>
                             <button className="button" type="button" value="Submit">Submit</button>
                             <br/>
-                            <p id="result"></p>
+                            <p className="output" id="result"></p>
                         </div>
                     </form>
                 </div>
